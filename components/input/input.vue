f1d3208402d315037ee0c5819f7c75f9ecdc3106
vccyb <13170906656@163.com>
2023-04-06 21:28:02 +0800

    chore: update input

--- components/input/input.vue
+++ components/input/input.vue
@@ -2,6 +2,13 @@
 import { computed } from 'vue'
 
 const props = defineProps({
+  type: {
+    type: String,
+    default: 'text',
+    validator: (value: string) => {
+      return ['text', 'textarea'].includes(value)
+    },
+  },
   disabled: {
     type: Boolean,
     default: false,
@@ -10,19 +17,71 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  size: {
+    type: String,
+    default: '',
+    validator: (value: string) => {
+      return ['medium', 'small'].includes(value)
+    },
+  },
+  clearable: {
+    type: Boolean,
+    default: false,
+  },
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
 })
+
+const emit = defineEmits(['update:modelValue'])
 
 const styleClass = computed(() => {
   return {
     'is-disabled': props.disabled,
     'is-center': props.center,
+    [`y-input--${props.size}`]: props.size,
   }
 })
+
+// 我们是没有办法修改props的，需要执行函数
+const clearText = () => {
+  emit('update:modelValue', '')
+}
 </script>
 
 <template>
   <div>
-    <input type="text" class="y-input" :class="styleClass" :disabled="props.disabled">
+    <template v-if="type === 'textarea'">
+      <div class="y-textarea-outer">
+        <textarea
+          v-bind="$attrs"
+          class="y-textarea"
+          :class="styleClass"
+          :disabled="props.disabled"
+          :value="modelValue"
+          @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        />
+        <span v-if="props.clearable && modelValue" class="y-texearea-clear" @click="clearText">
+          <y-icon name="close" />
+        </span>
+      </div>
+    </template>
+    <template v-else>
+      <div class="y-input-outer">
+        <input
+          v-bind="$attrs"
+          type="text" class="y-input"
+          :class="styleClass"
+          :disabled="props.disabled"
+          :value="modelValue"
+          @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        >
+        <span v-if="props.clearable && modelValue" class="y-input-clear" @click="clearText">
+          <y-icon name="close" />
+        </span>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -62,5 +121,75 @@ const styleClass = computed(() => {
 
   .y-input.is-center {
     text-align: center;
+  }
+
+  .y-textarea {
+    display: block;
+    resize: vertical;
+    padding: 5px 15px;
+    line-height: 1.5;
+    box-sizing: border-box;
+    width: 100%;
+    font-size: inherit;
+    color: #606266;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #dcdfe6;
+    border-radius: 4px;
+    transition: border-color .2s cubic-bezier(.645, .045, .355, 1)
+  }
+
+  .y-textarea.is-disabled {
+    background-color: #f5f7fa;
+    border-color: #e4e7ed;
+    color: #c0c4cc;
+    cursor: not-allowed;
+  }
+
+  .y-textarea.is-center {
+    text-align: center;
+  }
+
+  .y-textarea:focus {
+    outline: none;
+    border-color: #409eff;
+  }
+
+  .y-textarea-outer {
+    width: 100%;
+    position: relative;
+
+    .y-texearea-clear {
+      position: absolute;
+      right: 5%;
+      top: 50%;
+      transform: translateY(-50%);
+      color: #ccc;
+      cursor: pointer;
+    }
+  }
+
+  .y-input--medium {
+    height: 36px;
+    line-height: 36px;
+  }
+
+  .y-input--small {
+    height: 32px;
+    line-height: 32px;
+  }
+
+  .y-input-outer {
+    position: relative;
+    width: 100%;
+
+    .y-input-clear {
+      position: absolute;
+      right: 5%;
+      top: 50%;
+      transform: translateY(-50%);
+      color: #ccc;
+      cursor: pointer;
+    }
   }
 </style>
--- site/demos/input/basic.vue
+++ site/demos/input/basic.vue
@@ -3,7 +3,13 @@
 </docs>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const msg = ref('aaa')
+const msg2 = ref('bbb')
+
+const msg3 = ref('ccc')
+const msg4 = ref('ddd')
 </script>
 
 <template>
@@ -11,6 +17,34 @@
     <y-input />
     <y-input disabled />
     <y-input center />
+    <y-input placeholder="please enter" />
+  </div>
+  <div class="display-box">
+    <y-input v-model="msg" />
+    <span>{{ `msg:${msg}` }}</span>
+  </div>
+
+  <div class="display-box">
+    <y-input v-model="msg2" type="textarea" />
+    <span>{{ `msg:${msg2}` }}</span>
+  </div>
+
+  <div class="display-box">
+    <y-input type="text" placeholder="默认large" />
+    <y-input type="text" size="medium" placeholder="medium" />
+    <y-input type="text" size="small" placeholder="small" />
+  </div>
+
+  <div class="display-box">
+    <y-input type="textarea" disabled />
+    <y-input type="textarea" center />
+    <y-input type="textarea" />
+  </div>
+
+  <div class="display-box">
+    <y-input v-model="msg3" type="input" clearable />
+    <y-input v-model="msg4" type="textarea" clearable />
+    <y-input v-model="msg3" type="input" placeholder="12345" clearable />
   </div>
 </template>
 
@@ -18,5 +52,6 @@
 .display-box {
   display: flex;
   gap: 10px;
+  margin-bottom: 20px;
 }
 </style>
